163a107bd6486847ac1c387597d3503afea16559
zunait <zunaithossainratul@gmail.com>
2025-07-10 15:23:10 +0000

    Add files via upload

---
+++ Patterns/better_palindromic_diamond.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter row number = ");
+    scanf("%d",&n);
+    
+    //taking only odd values as input
+    if(n%2==0){
+        printf("Please enter an odd number!\n\n");
+        return 0;
+    }
+    
+    // for upper part of the diamond
+    int var = 0;
+    int mid = (n+1)/2;
+    
+    for(int i=1; i<=mid; i++){
+        // for spaces
+        for(int j=1; j<=(mid-i); j++){
+            printf(" ");
+        }
+        // for stars
+        var++;
+        int temp = var;
+        for(int k=1; k<=(2*i)-1; k++){
+            if(k<i){
+                printf("%d",temp);
+                temp++;
+            }else{
+                printf("%d",temp);
+                temp--;
+            }
+        }
+        
+        // for lines
+        printf("\n");
+    }
+    
+    // now lower part of the diamond
+    
+    int lower_mid = (n-1)/2;
+    for(int i=lower_mid; i>=1; i--){
+        
+        // for spaces
+        for(int j=lower_mid; j>=i; j--){
+            printf(" ");
+        }
+        // for stars
+        int temp = i;
+        for(int k=1; k<=(2*i)-1; k++){
+            if(k<i){
+                printf("%d",temp);
+                temp++;
+            }else{
+                printf("%d",temp);
+                temp--;
+            }
+        }
+
+        // for lines
+        printf("\n");
+    }
+
+    return 0;
+}
