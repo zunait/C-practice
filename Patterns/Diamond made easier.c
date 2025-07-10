0eb3460aabf2f5786d6af078ce3db0a24fc4cbf6
zunait <zunaithossainratul@gmail.com>
2025-07-10 14:10:06 +0000

    Add files via upload

---
+++ Patterns/Diamond made easier.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter row number = ");
+    scanf("%d",&n);
+
+    //taking only odd values as input
+    if(n%2==0){
+        printf("Please enter an odd number!\n\n");
+        return 0;
+    }
+    // for upper part of the diamond
+    int mid = (n+1)/2;
+    for(int i=1; i<=mid; i++){
+        // for spaces
+        for(int j=1; j<=(mid-i); j++){
+            printf(" ");
+        }
+        // for stars
+        for(int k=1; k<=(2*i)-1; k++){
+              printf("*");
+          }
+        // for lines
+        printf("\n");
+    }
+
+    //now lower part of the diamond
+    int lower_mid = (n-1)/2;
+    for(int i=1; i<=lower_mid; i++){
+
+        // for spaces
+        for(int j=1; j<=i; j++){
+            printf(" ");
+        }
+
+        // for stars
+        for(int k=(2*i)-1; k<=(n-2); k++){
+            printf("*");
+        }
+
+        // for lines
+        printf("\n");
+    }
+
+    return 0;
+}
